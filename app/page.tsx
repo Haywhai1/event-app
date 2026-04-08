2b712918ae7d8910bd382d42af5ae0f1fc3e5e04
Olawale Ayobami <ayobamiolawale016@gmail.com>
2026-04-08 17:40:14 +0100

    added explorePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import ExplorePage from "./explore/page";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-red-600 font-bold underline">Hello world!</h1>
+      <ExplorePage />
     </div>
   );
 }
